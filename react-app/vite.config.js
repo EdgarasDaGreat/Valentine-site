0077916f0dcfcba765f4b45d9f9641ae5b611abf
Edgaras <edgaras.daugevicius@stud.vilniustech.lt>
2026-02-09 21:36:31 +0200

    second

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/creditCalc/', // Ensures relative paths for assets
+  base: '/Valentine-site/', // Ensures relative paths for assets
   plugins: [react()],
 })
